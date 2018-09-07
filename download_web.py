93b543aee376b54df5f31c4fa6ff59bd978ac4a8
MyPoZi <kxx555u.u@gmail.com>
2018-09-07 21:00:11 +0900

    add read_csv

--- download_web.py
+++ download_web.py
@@ -1,18 +1,46 @@
 import urllib.request
 import datetime
+import pandas as pd
+import sys
+from requests import HTTPError
 
-def download():
+
+def download_csv():
     url = "http://namaristats.com/datatable.csv"
     date = datetime.date.today().strftime("%Y%m%d")
     savename = "lang" + date + ".csv"
-    print(savename)
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
     }
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', headers["User-Agent"])]
     urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(url, savename)
+    try:
+        urllib.request.urlretrieve(url, savename)
+        print(savename)
+        return savename
+    except (OSError, HTTPError) as e:
+        print("ERROR")
+        sys.exit()
+
+
+def read_csv(data):
+    csv_lst = pd.read_csv(data).values.tolist()
+    program_language = list()
+    program_rep = list()
+
+    for i in range(0, 19):
+        program_language.append(csv_lst[i][0])
+        program_rep.append(csv_lst[i][1])
+    return program_language, program_rep
+
+
+def main():
+    savename = download_csv()
+    program_language, program_rep = read_csv(savename)
+    print(program_language)
+    print(program_rep)
+
 
 if __name__ == '__main__':
-    download()
+    main()
