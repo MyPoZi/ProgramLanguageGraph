95b807bae37f7880b15c66404d63d1a09b8cf7f5
MyPoZi <kxx555u.u@gmail.com>
2018-09-07 22:03:14 +0900

    add insert_in_database

--- src/download_web.py
+++ src/download_web.py
@@ -2,13 +2,34 @@ import urllib.request
 import datetime
 import pandas as pd
 import sys
+import sqlite3
 from requests import HTTPError
+
+dbpath = '../db/language.db'
+
+
+def insert_in_database(language, rep):
+    date = datetime.date.today().strftime("%Y/%m/%d")
+
+    connection = sqlite3.connect(dbpath)
+    cursor = connection.cursor()
+    for (l, r) in zip(language, rep):
+        sql = 'INSERT INTO LANGUAGE (time, language, value) values (?,?,?)'
+        value = (date, l, r)
+        try:
+            cursor.execute(sql, value)
+
+        except sqlite3.Error as e:
+            print('sqlite3.Error occurred:', e.args[0])
+
+    connection.commit()
+    connection.close()
 
 
 def download_csv():
     url = "http://namaristats.com/datatable.csv"
     date = datetime.date.today().strftime("%Y%m%d")
-    savename = "lang" + date + ".csv"
+    savename = "../data/"+"lang" + date + ".csv"
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
     }
@@ -40,6 +61,7 @@ def main():
     program_language, program_rep = read_csv(savename)
     print(program_language)
     print(program_rep)
+    insert_in_database(program_language, program_rep)
 
 
 if __name__ == '__main__':
